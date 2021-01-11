6263819cba0b040c33b1d84b52c6ec8eb71ff55f
Jonathan <jonny@mondago.com>
2021-01-11 12:00:04 +0000

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,11 @@ class MyDocument extends Document {
         <body className="bg-white text-gray-900">
           <Main />
           <NextScript />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "0092687451534aa18387c263f4732039"}'
+          ></script>
         </body>
       </Html>
     );
